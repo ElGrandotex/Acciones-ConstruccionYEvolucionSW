2161a01e8b136dca67cb1012f7b8b2d2fc64b61c
ElGrandotex <jose.verdezoto.moran@gmail.com>
2023-11-25 10:03:52 -0500

    Interfaz
    Modificacion

--- src/app/acciones/interfaz/accion.interface.ts
+++ src/app/acciones/interfaz/accion.interface.ts
@@ -1,15 +1,15 @@
 export interface Busqueda {
-  "Global Quote": Dato;
+  "Global Quote": Accion;
 }
 
-export interface Dato {
+export interface Accion {
   "01. symbol":             string;
   "02. open":               string;
   "03. high":               string;
   "04. low":                string;
   "05. price":              string;
   "06. volume":             string;
-  "07. latest trading day": Date;
+  "07. latest trading day": string;
   "08. previous close":     string;
   "09. change":             string;
   "10. change percent":     string;
