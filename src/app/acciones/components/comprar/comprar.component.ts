253a4200d2bb0b82bb6eec4912ed55ba08c5f744
ElGrandotex <jose.verdezoto.moran@gmail.com>
2023-11-27 23:53:27 -0500

    Lista de compras
    metodo Agregar a la lista de compras

--- src/app/acciones/components/comprar/comprar.component.ts
+++ src/app/acciones/components/comprar/comprar.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { AccionesService } from '../../services/acciones.service';
+import { Compras } from '../../interfaz/compras.interface';
 
 @Component({
   selector: 'acc-comprar',
@@ -17,12 +17,27 @@ export class ComprarComponent {
   comprar(){
     console.log(this.volumen.nativeElement.value);
     const volumen = this.volumen.nativeElement.value;
-    this.accionesService.calcularCompra(parseFloat(volumen))
-    this.volumen.nativeElement.value = ''
+    this.accionesService.calcularCompra(parseFloat(volumen));
+    this.volumen.nativeElement.value = '';
+
+    const nuevaCompra: Compras = {
+      'Id': this.accionesService.id,
+      'Nombre': this.accionesService.listaAcciones['01. symbol'],
+      'Fecha' : this.accionesService.fechaCompra,
+      'PrecioCompra' : this.accionesService.listaAcciones['05. price'],
+      'Volumen' : parseInt(this.volumen.nativeElement.value),
+      'Total' : this.accionesService.valor
+    };
+
+    this.accionesService.agregarCompra(nuevaCompra);
+  }
+
+  get mostrarInput(){
+    return this.accionesService.mostradorTabla;
   }
 
   get mostrarCompra(){
-    return this.accionesService.mostradorTabla;
+    return this.accionesService.mostrarCompra;
   }
 
 }
